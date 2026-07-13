ab9e2e022070e0aa37df41966352f204cb905ac0
psa1582 <psa1582@g.skku.edu>
2026-07-14 00:16:45 +0900

    Add member profile photos

--- src/data/members.ts
+++ src/data/members.ts
@@ -11,6 +11,8 @@ export const professor: Member = {
   name: "Jeongwoo Park",
   role: "Professor",
   email: "jeffjw@g.skku.edu",
+  photo: "members/jeongwoo-park.jpg",
+  photoAlt: "Jeongwoo Park portrait",
   interests: ["Digital VLSI", "Intelligent accelerators", "Neuromorphic systems"],
 };
 
@@ -19,6 +21,8 @@ export const phdStudents: Member[] = [
     name: "Seungheon Baek",
     role: "PhD / Combined MS-PhD Student",
     email: "qwdqwd00@g.skku.edu",
+    photo: "members/seungheon-baek.jpg",
+    photoAlt: "Seungheon Baek portrait",
     interests: ["Neuromorphic Systems"],
   },
   {
@@ -31,18 +35,24 @@ export const phdStudents: Member[] = [
     name: "Jongyeop Kim",
     role: "PhD / Combined MS-PhD Student",
     email: "jondyp03@g.skku.edu",
+    photo: "members/jongyeop-kim.jpg",
+    photoAlt: "Jongyeop Kim portrait",
     interests: ["Accelerator for Transformers", "Neuromorphic Systems"],
   },
   {
     name: "Jinhyeong Park",
     role: "PhD / Combined MS-PhD Student",
     email: "irudajin@g.skku.edu",
+    photo: "members/jinhyeong-park.jpg",
+    photoAlt: "Jinhyeong Park portrait",
     interests: ["Network-on-Chip", "AI Accelerators"],
   },
   {
     name: "Younggil Jeong",
     role: "PhD / Combined MS-PhD Student",
     email: "j168816@g.skku.edu",
+    photo: "members/younggil-jeong.jpg",
+    photoAlt: "Younggil Jeong portrait",
     interests: ["Network-on-Chip", "AI Accelerators"],
   },
   {
@@ -64,18 +74,24 @@ export const msStudents: Member[] = [
     name: "Donghyun Nam",
     role: "MS Student",
     email: "nam3918@g.skku.edu",
+    photo: "members/donghyun-nam.jpg",
+    photoAlt: "Donghyun Nam portrait",
     interests: ["Neural Processing Unit"],
   },
   {
     name: "Jaeyoung Gong",
     role: "MS Student",
     email: "danieljk1119@gmail.com",
+    photo: "members/jaeyoung-gong.jpg",
+    photoAlt: "Jaeyoung Gong portrait",
     interests: ["Neural Processing Unit"],
   },
   {
     name: "Sanghoo Lee",
     role: "MS Student",
     email: "tkdgn2907@gmail.com",
+    photo: "members/sanghoo-lee.jpg",
+    photoAlt: "Sanghoo Lee portrait",
     interests: ["ML-based VLSI Design"],
   },
   {
